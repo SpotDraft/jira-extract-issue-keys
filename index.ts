8987ae1cb67c59951bd3c5d0fcca3533c06dc11d
dheerajmav <dheeraj.mavilla@spotdraft.com>
2023-04-25 13:14:18 +0530

    update

--- index.ts
+++ index.ts
@@ -100,7 +100,7 @@ async function extractJiraKeysFromCommit() {
         }
 
     } catch (error) {
-        core.setFailed(error.message);
+        core.setFailed((error as Error).message);
     }
 }
 
